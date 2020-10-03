bf6ef2fdd2fac27cfa874f494857d3127a7ee161
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-03 07:28:09 +0000

    >  compile NEMU 3019244249 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:28:09 up  6:41,  1 user,  load average: 0.26, 0.18, 0.13 94a9e8cee2eabafb18702d80817540bb1160b710

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -189,7 +189,7 @@ int eval(int p, int q){
           case '-':return val1-val2;
           case '*':return val1*val2;
           case '/':return val1/val2;
-          default:assert(0);
+          default:return -1;
         }
       }
     }
