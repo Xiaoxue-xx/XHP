491f0d1c752576f994c010be3623c52d63e3c1f0
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 02:09:01 +0000

    >  compile NEMU 3019244249 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:09:01 up  2:01,  1 user,  load average: 0.19, 0.08, 0.07 c864c3ced410785848db85789a919999ccf25e8

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,3 +1,4 @@
+#include <regex.h>
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
 
@@ -20,4 +21,69 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* new_wp(char *string) {
+	WP *new;
+	bool success;
+	if(free_==NULL){
+		printf("Error!free is null!\n");
+		assert(0);
+	}
+	new=free_;
+	free_=free_->next;
+	strcpy(new->str,string);
+	new->value=expr(string,&success);
+
+	int status;
+	int cflags =REG_EXTENDED;
+	regmatch_t pmatch[1];
+	const size_t nmatch=1;
+	regex_t  reg;
+	const char * pattern="\\$[eE][iI][pP][=][=]0x[0-9a-fA-F]{1,8}";
+	regcomp(&reg,pattern,cflags);
+	status=regexec(&reg,string,nmatch,pmatch,0);
+	if(status==REG_NOMATCH)
+		new->type='w';
+	else if(status==0)
+		new->type='b';
+
+	new->Enb='Y';
+	new->next=head;
+	head=new;
+	printf("The number is %d\n",new->NO);
+	return new;
+}
+
+void free_wp(int number){
+	WP *p,*q;
+	q=NULL;
+	p=head;
+	while(p!=NULL&&p->NO!=number){
+		q=p;
+		p=p->next;
+	}
+	if(p==NULL){
+		printf("Can't find!\n");
+		assert(0);
+	};
+ 	if(q==NULL){
+		head=head->next;
+		p->value=0;
+		memset(p->str,0,sizeof(p->str));
+		p->type=' ';
+		p->Enb='N';
+		p->next=free_;
+		free_=p;
+	}
+	else{
+		q->next=p->next;
+		p->value=0;
+		memset(p->str,0,sizeof(p->str));
+		p->type=' ';
+		p->Enb='N';
+		p->next=free_;
+		free_=p;
+	}
+	printf("Free the %d\n",p->NO);
+	return;
+}
 
