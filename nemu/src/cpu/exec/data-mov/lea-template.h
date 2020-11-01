a862eb009bde5298383b0bce62d4d0e1f4aba599
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-01 12:58:56 +0000

    >  compile NEMU 3019244249 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:58:56 up  4:47,  1 user,  load average: 0.27, 0.18, 0.12 bbfa8624154d22f977bd07bc3d566a63c0d8a994

--- nemu/src/cpu/exec/data-mov/lea-template.h
+++ nemu/src/cpu/exec/data-mov/lea-template.h
@@ -3,8 +3,11 @@
 #define instr lea
 
 static void do_execute(){
-	DATA_TYPE addr = op_src->reg;
-	OPERAND_W(op_dest,addr);
+	uint32_t tmp = op_src->addr;
+	#if DATA_BYTE ==2
+		tmp &=0xffff;
+	#endif
+	OPERAND_W(op_dest,tmp);
 	print_asm_template2();
 }
 make_instr_helper(rm2r)
