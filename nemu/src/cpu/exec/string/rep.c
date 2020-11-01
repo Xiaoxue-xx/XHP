0264b5d06c8fbbd8cc70a2701ee96aaa29f996ef
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-01 12:42:52 +0000

    >  compile NEMU 3019244249 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:42:52 up  4:31,  1 user,  load average: 0.10, 0.07, 0.08 959c270b7df6a29f34abc6f15782dafaf90823

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -25,7 +25,7 @@ make_helper(rep) {
 				|| ops_decoded.opcode == 0xaf	// scasw
 				);
 
-			if(cpu.ZF==1)break;
+			if(cpu.ZF==0)break;
 			/* TODO: Jump out of the while loop if necessary. */
 
 		}
@@ -54,7 +54,7 @@ make_helper(repnz) {
 			  );
 
 		/* TODO: Jump out of the while loop if necessary. */
-		if(cpu.ZF==0)break;
+		if(cpu.ZF==1)break;
 	}
 
 #ifdef DEBUG
