bdeed7a8ad6b1cb53f42afdb72a72ae389c4d86a
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-02 12:47:17 +0000

    >  compile NEMU 3019244249 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:47:17 up  3:28,  1 user,  load average: 0.19, 0.15, 0.13 394cef1971fe6f406c8aa5674cff09d78055f772

--- nemu/src/cpu/exec/control/jns-template.h
+++ nemu/src/cpu/exec/control/jns-template.h
@@ -5,8 +5,13 @@
 
 static void do_execute() {
 	DATA_TYPE_S displacement = op_src->val;
-	print_asm("jns %x",cpu.eip+1+DATA_BYTE+displacement);
-	if(cpu.SF ==0 )cpu.eip+=displacement;
+//	print_asm("jns %x",cpu.eip+1+DATA_BYTE+displacement);
+	if(cpu.SF ==0 ){cpu.eip+=displacement;
+#if DATA_BYTE ==2
+	cpu.eip &= 0xffff;
+#endif
+}
+	print_asm("jns %x",cpu.eip+1+DATA_BYTE);
 }
 make_instr_helper(i)
 
--- nemu/src/cpu/exec/control/js-template.h
+++ nemu/src/cpu/exec/control/js-template.h
@@ -4,8 +4,13 @@
 
 static void do_execute(){
 	DATA_TYPE_S displacement = op_src->val;
-	print_asm("js %x",cpu.eip+1+DATA_BYTE+displacement);
-	if(cpu.SF == 1)cpu.eip+=displacement;
+//	print_asm("js %x",cpu.eip+1+DATA_BYTE+displacement);
+	if(cpu.SF == 1){cpu.eip+=displacement;
+#if DATA_BYTE ==2
+	cpu.eip &=0xffff;
+#endif
+}
+	print_asm("js %x",cpu.eip+1+DATA_BYTE);
 }
 
 make_instr_helper(i)
