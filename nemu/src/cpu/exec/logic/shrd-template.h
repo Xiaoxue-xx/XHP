6b4ff8cabccc95467dca721e51121a117c9ef35f
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 06:04:37 +0000

    >  compile NEMU 3019244249 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:04:37 up 36 min,  1 user,  load average: 0.28, 0.35, 0.25 dce11ea4dd78eb96ff5020774b98175ec90f2156

--- nemu/src/cpu/exec/logic/shrd-template.h
+++ nemu/src/cpu/exec/logic/shrd-template.h
@@ -21,12 +21,35 @@ static void do_execute () {
 	print_asm("shrd" str(SUFFIX) " %s,%s,%s", op_src->str, op_dest->str, op_src2->str);
 }
 
-make_helper(concat(shrdi_, SUFFIX)) {
+make_helper(concat(shrd_i_, SUFFIX)) {
 	int len = concat(decode_si_rm2r_, SUFFIX) (eip + 1);  /* use decode_si_rm2r to read 1 byte immediate */
 	op_dest->val = REG(op_dest->reg);
 	do_execute();
 	return len + 1;
 }
+
+/*static void do_execute_cl () {
+	DATA_TYPE in=op_dest->val;
+	DATA_TYPE out=op_src->val;
+	uint8_t count=reg_b (R_CL);
+	count &= 0x1f;
+	while(count!=0){
+		out >>=1;
+		out |=(in&1)<<((DATA_BYTE<<3)-1);
+		in >>=1;
+		count--;
+	}
+
+	OPERAND_W(op_src,out);
+
+	print_asm("shrd" str(SUFFIX) "$cl,%s,%s", op_dest->str,op_src->str);
+}
+
+make_helper(concat(shrd_cl_, SUFFIX)) {
+	int len =concat(decode_rm2r_,SUFFIX)(eip+1);
+	do_execute_cl();
+	return len+1;
+}*/
 #endif
 
 #include "cpu/exec/template-end.h"
