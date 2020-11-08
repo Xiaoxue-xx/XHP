554aef14380713af8bcf60f5d3764e80ab741f45
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-08 10:48:21 +0000

    >  run 3019244249 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:48:21 up  1:34,  1 user,  load average: 0.34, 0.26, 0.19 6d3c31f4e18324792c43176ce19cf1477cb5c26

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -3,12 +3,12 @@
 #include "monitor/watchpoint.h"
 #include "nemu.h"
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
 void cpu_exec(uint32_t);
+void display_reg();
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 char* rl_gets() {
@@ -28,68 +28,128 @@ char* rl_gets() {
 	return line_read;
 }
 
+/* TODO: Add single step */
+static int cmd_si(char *args) {
+	char *arg = strtok(NULL, " ");
+	int i = 1;
+
+	if(arg != NULL) {
+		sscanf(arg, "%d", &i);
+	}
+	cpu_exec(i);
+	return 0;
+}
+
+/* TODO: Add info command */
+static int cmd_info(char *args) {
+	char *arg = strtok(NULL, " ");
+
+	if(arg != NULL) {
+		if(strcmp(arg, "r") == 0) {
+			display_reg();
+		}
+		else if(strcmp(arg, "w") == 0) {
+			list_watchpoint();
+		}
+	}
+	return 0;
+}
+
+/* Add examine memory */
+static int cmd_x(char *args) {
+	char *arg = strtok(NULL, " ");
+	int n;
+	swaddr_t addr;
+	int i;
+
+	if(arg != NULL) {
+		sscanf(arg, "%d", &n);
+
+		bool success;
+		addr = expr(arg + strlen(arg) + 1, &success);
+		if(success) { 
+			for(i = 0; i < n; i ++) {
+				if(i % 4 == 0) {
+					printf("0x%08x: ", addr);
+				}
+
+				printf("0x%08x ", swaddr_read(addr, 4));
+				addr += 4;
+				if(i % 4 == 3) {
+					printf("\n");
+				}
+			}
+			printf("\n");
+		}
+		else { printf("Bad expression\n"); }
+
+	}
+	return 0;
+}
+
+/* Add expression evaluation  */
+static int cmd_p(char *args) {
+	bool success;
+
+	if(args) {
+		uint32_t r = expr(args, &success);
+		if(success) { printf("0x%08x(%d)\n", r, r); }
+		else { printf("Bad expression\n"); }
+	}
+	return 0;
+}
+
+/* Add set watchpoint  */
+static int cmd_w(char *args) {
+	if(args) {
+		int NO = set_watchpoint(args);
+		if(NO != -1) { printf("Set watchpoint #%d\n", NO); }
+		else { printf("Bad expression\n"); }
+	}
+	return 0;
+}
+
+/* Add delete watchpoint */
+static int cmd_d(char *args) {
+	int NO;
+	sscanf(args, "%d", &NO);
+	if(!delete_watchpoint(NO)) {
+		printf("Watchpoint #%d does not exist\n", NO);
+	}
+
+	return 0;
+}
+
+/* Add display backtrace */
+static int cmd_bt(char *args) {
+	const char* find_fun_name(uint32_t eip);
+	struct {
+		swaddr_t prev_ebp;
+		swaddr_t ret_addr;
+		uint32_t args[4];
+	} sf;
+
+	uint32_t ebp = cpu.ebp;
+	uint32_t eip = cpu.eip;
+	int i = 0;
+	while(ebp != 0) {
+		sf.args[0] = swaddr_read(ebp + 8, 4);
+		sf.args[1] = swaddr_read(ebp + 12, 4);
+		sf.args[2] = swaddr_read(ebp + 16, 4);
+		sf.args[3] = swaddr_read(ebp + 20, 4);
+
+		printf("#%d 0x%08x in %s (0x%08x 0x%08x 0x%08x 0x%08x)\n", i, eip, find_fun_name(eip), sf.args[0], sf.args[1], sf.args[2], sf.args[3]);
+		i ++;
+		eip = swaddr_read(ebp + 4, 4);
+		ebp = swaddr_read(ebp, 4);
+	}
+	return 0;
+}
+
+
 static int cmd_c(char *args) {
 	cpu_exec(-1);
 	return 0;
-}
-
-static int cmd_si(char *args) {
-       int step;
-       if(args==NULL) step=1;
-       else sscanf(args,"%d",&step);
-       cpu_exec(step);
-       return 0;
-}
-
-static int cmd_info(char *args) {
-       if(args[0]=='r') {
-            int i;
-            for(i=R_EAX; i<=R_EDI; i++) {
-               printf("$%s\t0x%08x\n",regsl[i],reg_l(i));
-            }
-            printf("$eip\t0x%08x\n",cpu.eip);
-       }
-       if(args[0]=='w') {
-		print_wp();
-	}
-       return 0;
-}
-
-static int cmd_x(char *args) {
-       if(args==NULL){
-       printf("Fail!/n");
-       return 0;
-       }
-       int n,EXPR;
-       sscanf(args,"%d%x",&n,&EXPR);
-       int i;
-       for(i=0;i<n;i++){
-          printf("0x%8x 0x%x\n",EXPR+i*5,swaddr_read(EXPR+i*5,4));
-       }
-       return 0;
-}
-static int cmd_p(char *args) {
-       bool success;
-       int i=expr(args,&success);
-	printf("%d\n",i);
-	return 0;
-}
-static int cmd_w(char *args){
-	if(args==NULL)
-		printf("Invalid command!\n");
-	else
-		new_wp(args);
-	return 0;
-}
-
-static int cmd_d(char *args){
-	if(args==NULL)
-		printf("Invalid command!\n");
-	else{
-		int number=atoi(args);
-		free_wp(number);
-	}
-	return 0;	
 }
 
 static int cmd_q(char *args) {
@@ -105,14 +165,16 @@ static struct {
 } cmd_table [] = {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
-	{ "q", "Exit NEMU", cmd_q },
-        { "si", "Step one instruction exactly",cmd_si },
-        { "info", "Print information",cmd_info },
-        { "x", "Examine 4*N bytes at address",cmd_x },
-        { "p", "Evaluate expression",cmd_p},
-        { "w", "Set watch point",cmd_w},
-	{ "d", "Delete watch point",cmd_d},
+	{ "q", "Exit NEMU", cmd_q }, 
+
 	/* TODO: Add more commands */
+        { "si", "Single step", cmd_si },
+        { "info", "info r - print register values; info w - show watch point state", cmd_info },
+	{ "x", "Examine memory", cmd_x },
+        { "p", "Evaluate the value of expression", cmd_p },
+	{ "w", "Set watchpoint", cmd_w },
+	{ "d", "Delete watchpoint", cmd_d },
+	{ "bt", "Display backtrace", cmd_bt }
 
 };
 
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,6 +1,6 @@
-#include <regex.h>
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
+#include <stdlib.h>
 
 #define NR_WP 32
 
@@ -21,91 +21,73 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
-WP* new_wp(char *string) {
-	WP *new;
+static WP* new_WP() {
+	assert(free_ != NULL);
+	WP *p = free_;
+	free_ = free_->next;
+	return p;
+}
+
+static void free_WP(WP *p) {
+	assert(p >= wp_pool && p < wp_pool + NR_WP);
+	free(p->expr);
+	p->next = free_;
+	free_ = p;
+}
+
+int set_watchpoint(char *e) {
+	uint32_t val;
 	bool success;
-	if(free_==NULL){
-		printf("Error!free is null!\n");
-		assert(0);
-	}
-	new=free_;
-	free_=free_->next;
-	strcpy(new->str,string);
-	new->value=expr(string,&success);
+	val = expr(e, &success);
+	if(!success) return -1;
 
-	int status;
-	int cflags =REG_EXTENDED;
-	regmatch_t pmatch[1];
-	const size_t nmatch=1;
-	regex_t  reg;
-	const char * pattern="\\$[eE][iI][pP][=][=]0x[0-9a-fA-F]{1,8}";
-	regcomp(&reg,pattern,cflags);
-	status=regexec(&reg,string,nmatch,pmatch,0);
-	if(status==REG_NOMATCH)
-		new->type='w';
-	else if(status==0)
-		new->type='b';
+	WP *p = new_WP();
+	p->expr = strdup(e);
+	p->old_val = val;
 
-	new->Enb='Y';
-	new->next=head;
-	head=new;
-	printf("The number is %d\n",new->NO);
-	return new;
+	p->next = head;
+	head = p;
+
+	return p->NO;
 }
 
-void free_wp(int number){
-	WP *p,*q;
-	q=NULL;
-	p=head;
-	while(p!=NULL&&p->NO!=number){
-		q=p;
-		p=p->next;
+bool delete_watchpoint(int NO) {
+	WP *p, *prev = NULL;
+	for(p = head; p != NULL; prev = p, p = p->next) {
+		if(p->NO == NO) { break; }
 	}
-	if(p==NULL){
-		printf("Can't find!\n");
-		assert(0);
-	};
- 	if(q==NULL){
-		head=head->next;
-		p->value=0;
-		memset(p->str,0,sizeof(p->str));
-		p->type=' ';
-		p->Enb='N';
-		p->next=free_;
-		free_=p;
-	}
-	else{
-		q->next=p->next;
-		p->value=0;
-		memset(p->str,0,sizeof(p->str));
-		p->type=' ';
-		p->Enb='N';
-		p->next=free_;
-		free_=p;
-	}
-	printf("Free the %d\n",p->NO);
-	return;
+
+	if(p == NULL) { return false; }
+	if(prev == NULL) { head = p->next; }
+	else { prev->next = p->next; }
+
+	free_WP(p);
+	return true;
 }
 
-int judge_wp(){
-	WP* p;
-	bool success;
-	p=head;
-	if(p==NULL)
-		return 0;
-	while(p){
-		p->newvalue=expr(p->str,&success);
-		if(p->newvalue!=p->value)
-			return -1;
-		p=p->next;
+void list_watchpoint() {
+	if(head == NULL) {
+		printf("No watchpoints\n");
+		return;
 	}
-	return 0;
+
+	printf("%8s\t%8s\t%8s\n", "NO", "Address", "Enable");
+	WP *p;
+	for(p = head; p != NULL; p = p->next) {
+		printf("%8d\t%s\t%#08x\n", p->NO, p->expr, p->old_val);
+	}
 }
 
-void print_wp(){
-	printf("Num\tType\tEnb \t\t Value\t\t What\n");
-	int i;
-	for(i=0;i<NR_WP;i++){
-		printf("%4d\t%c\t%c\t\t0x%x\t%s\n",wp_pool[i].NO,wp_pool[i].type,wp_pool[i].Enb,wp_pool[i].value,wp_pool[i].str);
+WP* scan_watchpoint() {
+	WP *p;
+	for(p = head; p != NULL; p = p->next) {
+		bool success;
+		p->new_val = expr(p->expr, &success);
+		if(p->old_val != p->new_val) {
+			return p;
+		}
+	}
+
+	return NULL;
 }
-}
+
